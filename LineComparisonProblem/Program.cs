ec93505c81123e11f85e4ea3b20b6aeebeb2aca0
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-05 13:39:58 +0530

    Added to LineLength

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -1,1 +1,14 @@
 ﻿
+Console.WriteLine("Enter the co-ordinates of the line");
+Console.WriteLine("Enter the first co-ordinate");
+Console.Write("Enter value of x1 : ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter value of y1 : ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second co-ordinate");
+Console.Write("Enter value of x2 : ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter value of y2 : ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine("Length of the line is : " + length);
